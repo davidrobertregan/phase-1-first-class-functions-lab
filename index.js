57976990684b66c02de4c12d0ec4957b98151173
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-08-10 16:21:42 -0500

    commit tree efaa486ed8529cbbfd9277921d386c05f8ec7efc

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+// Code your solution in this file
+//const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice (0,2)
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fare) {
+    return function(num) {
+        return (fare * num)
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, fn) {
+    return fn(drivers);
+}
